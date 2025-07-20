605a27a747768079d048d41407f6cd8977727e3d
abdulahiredwann <abdulahiredwann@gmail.com>
2025-07-20 19:23:34 +0300

    Update index.html and App.tsx for improved SEO and navigation
    - Enhanced index.html with comprehensive meta tags for SEO, including Open Graph and Twitter card data.
    - Updated the title and description to reflect the Aqua ERP branding and functionality.
    - Modified App.tsx to set the default route to the Login component, improving user navigation experience.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,16 @@ import { Suspense } from "react";
 import { Outlet, Route, Routes } from "react-router-dom";
 import { Toaster } from "sonner";
 import { LoadingSpinner } from "./components/ui/loading-spinner";
+import Login from "./Pages/Login/Login";
 import NotFound from "./Pages/NotFound";
 import { adminRoutes } from "./routes/adminRoutes";
-import Login from "./Pages/Login/Login";
 import { cashierRoutes } from "./routes/cashierRoutes";
 function App() {
   return (
     <>
       <Suspense fallback={<LoadingSpinner size="lg" />}>
         <Routes>
+          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route
             element={
