96c9a263fe0f1fc36dd904ce7a302ab48e6d21ce
Fuad Jemal <fuad47722@gmail.com>
2026-02-21 10:41:29 +0300

    update the api

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -4,13 +4,13 @@ const isDevelopment = process.env.NODE_ENV === "development";
 const api = axios.create({
   baseURL: isDevelopment
     ? "http://localhost:4000/api"
-    : "https://aqua.selamdca.org/api",
+    : "https://shieradevelopers.com/aquaErp",
   withCredentials: true,
 });
 
 const nPoint = isDevelopment
   ? "http://localhost:4000/"
-  : "https://aqua.selamdca.org/";
+  : "https://shieradevelopers.com/";
 
 export default api;
 export { CanceledError, nPoint };
